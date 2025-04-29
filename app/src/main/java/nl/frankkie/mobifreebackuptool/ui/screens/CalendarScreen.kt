31e80838707bcd4dc1a092d179ed80a55828fb96
frankbouwens <frankkie12345@gmail.com>
2025-04-29 23:28:22 +0200

    Add calendar

--- app/src/main/java/nl/frankkie/mobifreebackuptool/ui/screens/CalendarScreen.kt
+++ app/src/main/java/nl/frankkie/mobifreebackuptool/ui/screens/CalendarScreen.kt
@@ -1,24 +1,44 @@
 package nl.frankkie.mobifreebackuptool.ui.screens
 
+import android.content.pm.PackageManager
+import android.provider.CalendarContract
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import kotlinx.coroutines.launch
 import nl.frankkie.mobifreebackuptool.AppViewModel
+import nl.frankkie.mobifreebackuptool.appContext
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun CalendarScreen(navController: NavHostController, appViewModel: AppViewModel) {
     Scaffold(
@@ -41,8 +61,138 @@ fun CalendarScreen(navController: NavHostController, appViewModel: AppViewModel)
                     .padding(16.dp)
             ) {
                 Text(text = "Calendar Screen")
-                Text(text = "This is unfinished and will be added later.")
+                val calendarPermissionState =
+                    rememberPermissionState(android.Manifest.permission.READ_CALENDAR)
+                if (!calendarPermissionState.status.isGranted) {
+                    Button(onClick = { calendarPermissionState.launchPermissionRequest() }) {
+                        Text(text = "Request Calendar Permission")
+                    }
+                    return@Box
+                }
+
+                var calendarEventsLoading by remember { mutableStateOf(true) }
+                var calendarEvents by remember { mutableStateOf(emptyList<MyCalendarEvent>()) }
+                val scope = rememberCoroutineScope()
+
+                LaunchedEffect(null) {
+                    scope.launch {
+                        calendarEvents = loadCalendarEvents()
+                        calendarEventsLoading = false
+                    }
+                }
+
+                if (calendarEventsLoading) {
+                    Text(text = "Loading calendar events...")
+                    return@Box
+                }
+
+                if (calendarEvents.isEmpty()) {
+                    Text(text = "No calendar events found.")
+                    return@Box
+                }
+
+                Text(text = "Calendar events:")
+                LazyColumn {
+                    items(calendarEvents.size) { index ->
+                        val event = calendarEvents[index]
+                        ListItem(
+                            headlineContent = {
+                                Text(text = event.title ?: "No title")
+                            },
+                            supportingContent = {
+                                if (event.allDay) {
+                                    Text(text = "All day event")
+                                } else {
+                                    Text(text = "Start: ${event.startTime}; End: ${event.endTime}")
+                                }
+                            },
+                            leadingContent = {
+                                Icon(
+                                    Icons.Filled.DateRange,
+                                    contentDescription = "Calendar Event Icon",
+                                )
+                            }
+                        )
+                    }
+                }
             }
         }
     }
 }
+
+fun loadCalendarEvents(): List<MyCalendarEvent> {
+    val TAG = "MFBT"
+    if (ContextCompat.checkSelfPermission(
+            appContext,
+            android.Manifest.permission.READ_CALENDAR
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        return emptyList()
+    }
+
+    val cursor = appContext.contentResolver.query(
+        CalendarContract.Events.CONTENT_URI,
+        arrayOf(
+            CalendarContract.Events._ID,
+            CalendarContract.Events.TITLE,
+            CalendarContract.Events.DTSTART,
+            CalendarContract.Events.DTEND,
+            CalendarContract.Events.ALL_DAY,
+            CalendarContract.Events.EVENT_LOCATION,
+            CalendarContract.Events.DESCRIPTION
+        ),
+        null,
+        null,
+        null
+    )
+
+    val eventsList = mutableListOf<MyCalendarEvent>()
+    try {
+        cursor?.let { safeCursor ->
+            while (safeCursor.moveToNext()) {
+                val id = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(CalendarContract.Events._ID)
+                )
+                val title = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(CalendarContract.Events.TITLE)
+                )
+                val dtStart = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(CalendarContract.Events.DTSTART)
+                )
+                val dtEnd = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(CalendarContract.Events.DTEND)
+                )
+                val allDay = safeCursor.getInt(
+                    safeCursor.getColumnIndexOrThrow(CalendarContract.Events.ALL_DAY)
+                ) != 0
+                val location = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(CalendarContract.Events.EVENT_LOCATION)
+                )
+                val description = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(CalendarContract.Events.DESCRIPTION)
+                )
+                val event =
+                    MyCalendarEvent(id, title, dtStart, dtEnd, allDay, location, description)
+                eventsList.add(event)
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        Log.e(TAG, "Error reading calendar: ${e.message}", e)
+        return emptyList()
+    } finally {
+        cursor?.close()
+    }
+
+    return emptyList()
+}
+
+data class MyCalendarEvent(
+    val id: String?,
+    val title: String?,
+    val startTime: String?,
+    val endTime: String?,
+    val allDay: Boolean,
+    val location: String?,
+    val description: String?,
+)
--- app/src/main/java/nl/frankkie/mobifreebackuptool/ui/screens/ContactsScreen.kt
+++ app/src/main/java/nl/frankkie/mobifreebackuptool/ui/screens/ContactsScreen.kt
@@ -1,27 +1,42 @@
 package nl.frankkie.mobifreebackuptool.ui.screens
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
+import kotlinx.coroutines.launch
 import nl.frankkie.mobifreebackuptool.AppViewModel
+import nl.frankkie.mobifreebackuptool.appContext
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
@@ -55,8 +70,112 @@ fun ContactsScreen(navController: NavHostController, appViewModel: AppViewModel)
                     return@Box
                 }
 
+                var contactListLoading by remember {
+                    mutableStateOf(true)
+                }
+                var contactList by remember {
+                    mutableStateOf(emptyList<MyContact>())
+                }
+                val scope = rememberCoroutineScope()
+                LaunchedEffect(null) {
+                    scope.launch {
+                        contactList = readContacts()
+                        contactListLoading = false
+                    }
+                }
 
+                if (contactListLoading) {
+                    Text("Loading contacts...")
+                    return@Box
+                }
+
+                if (contactList.isEmpty()) {
+                    Text("No contacts found.")
+                    return@Box
+                }
+
+                Text("Contacts:")
+                LazyColumn {
+                    items(contactList.size) { index ->
+                        val contact = contactList[index]
+                        ListItem(
+                            headlineContent = {
+                                Text(text = contact.displayName)
+                            },
+                            supportingContent = {
+                                //Text(text = contact.phoneNumber)
+                                Text("Example phone number")
+                            },
+                            leadingContent = {
+                                Icon(
+                                    Icons.Filled.AccountCircle,
+                                    contentDescription = "Contact Icon",
+                                )
+                            }
+                        )
+                    }
+
+                }
             }
         }
     }
 }
+
+
+@SuppressLint("Range")
+fun readContacts(): List<MyContact> {
+    val TAG = "MFBT"
+    if (ContextCompat.checkSelfPermission(
+            appContext,
+            android.Manifest.permission.READ_CONTACTS
+        ) != PackageManager.PERMISSION_GRANTED
+    ) {
+        return emptyList()
+    }
+    //Permission is granted, read contacts
+    val cursor = appContext.contentResolver.query(
+        ContactsContract.Contacts.CONTENT_URI,
+        arrayOf(
+            ContactsContract.Contacts._ID,
+            ContactsContract.Contacts.DISPLAY_NAME,
+            //TODO: Get actual phone number, not just *if* there is a phone number.
+            ContactsContract.Contacts.HAS_PHONE_NUMBER
+        ),
+        null,
+        null,
+        null
+    )
+    val contactsList = mutableListOf<MyContact>()
+    try {
+        cursor?.let { safeCursor ->
+            while (safeCursor.moveToNext()) {
+                val id = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID)
+                )
+                val displayName = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME)
+                )
+                val phoneNumber = safeCursor.getString(
+                    safeCursor.getColumnIndexOrThrow(ContactsContract.Contacts.HAS_PHONE_NUMBER)
+                )
+                val contact = MyContact(id, displayName, phoneNumber)
+                contactsList.add(contact)
+            }
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        Log.e(TAG, "Error reading contacts: ${e.message}", e)
+        return emptyList()
+    } finally {
+        cursor?.close()
+    }
+    return contactsList
+}
+
+
+
+data class MyContact(
+    val id: String,
+    val displayName: String,
+    val phoneNumber: String
+)
